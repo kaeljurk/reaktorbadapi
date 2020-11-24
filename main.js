6717c30cc4196cbaf9feddeca7b4240c4e2a4333
cyborz <kaeljurk@gmail.com>
2020-11-24 12:31:54 +0200

    mains.js cleanup

--- main.js
+++ main.js
@@ -2,70 +2,6 @@ var categories = ['jackets', 'shirts', 'accessories'];
 var XMLParser = new DOMParser();
 var badApi;
 
-/*
-class BadApi {
-    constructor() {
-        this.endpoint = 'https://bad-api-assignment.reaktor.com';
-        this.cacheTime = 300000;
-        this.cache = {};
-    }
-
-    async getItemList(category) {
-        const itemList = await this.getRequestData(this.endpoint + '/products/' + category);
-        return itemList;
-    }
-
-    async getAvailability(manufacturer) {
-        const availabilityInfo = await this.getRequestData(this.endpoint + '/availability/' + manufacturer);
-        return availabilityInfo;
-    }
-
-    async getRequestData(url) {
-        const self = this;
-
-        if (self.cache[url] && !self.cacheExpired(self.cache[url])) {
-            return self.cache[url];
-        }
-
-        const response = await self.fetchRetry(url, [], 2);
-        const responseData = await response.json();
-        self.cache[url] = responseData;
-        self.cache[url].cacheTimestamp = Date.now();
-        return self.cache[url];
-    }
-
-    async fetchRetry(url, options, attempt) {
-        try {
-            const response = await fetch(url, options);
-
-            if (!response.ok) {
-                throw new Error("Unable to retrieve data from " + url);
-            }
-            return response;
-
-        } catch (error) {
-            if (attempt <= 1) {
-                throw error;
-            }
-            await this.sleep(1000);
-            return this.fetchRetry(url, options, attempt - 1);
-        }
-    }
-
-    cacheExpired(cacheItem) {
-        if (cacheItem.cacheTimestamp && cacheItem.cacheTimestamp + this.cacheTime > Date.now()) {
-            return false;
-        }
-        return true;
-    }
-
-    sleep(ms) {
-        return new Promise(resolve => setTimeout(resolve, ms))
-    }
-
-}
-
-*/
 
 init();
 
@@ -128,7 +64,7 @@ async function showItem(item) {
             const itemAvailability = XMLParser.parseFromString(itemAvailabilityXML.DATAPAYLOAD, "text/xml").getElementsByTagName("INSTOCKVALUE")[0].childNodes[0].nodeValue;
             modal.querySelector("#itemAvailabilityInfo").textContent = itemAvailability;
         } catch (error) {
-            modal.querySelector("#itemAvailabilityInfo").textContent = 'Error, reload page and try again.';
+            modal.querySelector("#itemAvailabilityInfo").textContent = 'Error, refresh the page and try again.';
         }
 
     }).catch(function (error) {
